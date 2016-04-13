e43fd9439be2d43ee11a252f6bcac9ba4da74a5d
marcnealer <marcnealer@gmail.com>
2016-04-13 15:11:00 +0700

    first commit

---
+++ promozo/core/models.py
@@ -0,0 +1,114 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth import get_user_model
+from django_extensions.db.fields.json import JSONField
+User=get_user_model()
+# Create your models here.
+
+class baseModel(models.Model):
+    """
+    This is an abstract model. No table is created for this, Instead it
+    lists fields that are added to other models, based on this one
+    """
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    updated_by = models.ForeignKey(User,related_name='updated_by')
+
+    class Meta:
+        abstract=True
+
+class UserDocuments(baseModel):
+    """
+    records used to store documents linked to a selected user
+    """
+    user =models.ForeignKey(User,related_name='document_owner')
+    name = models.CharField(max_length=200)
+    document = models.FileField(upload_to='userdocs')
+
+    def __str__(self):
+        return self.name
+
+class University(baseModel):
+    """
+    Model for University Objects
+    This includes links to University Staff and Students.
+    Fields
+
+    name : Charfield max 200 chars Required
+    logo : Image field, uploaded to /images/logos/. Not Requried
+    description : Text Field, Not Required
+    address : text Field required
+    universityStaff. M2M link to users
+    universityStudents M2M link to Students
+    """
+    name = models.CharField(max_length=200)
+    logo = models.ImageField(upload_to='images/logos',blank=True,null=True)
+    description = models.TextField(blank=True)
+    website = models.URLField()
+    address = models.TextField()
+    universityStaff = models.ManyToManyField(User,related_name='UniversityStaff',blank=True,null=True)
+    universityStudents = models.ManyToManyField(User,realated_name='UniversityStudents',blank=True,null=True)
+
+    def __str__(self):
+        return self.name
+
+class Region(baseModel):
+    """
+    Region objects class linked to multiple universities
+    """
+    name = models.CharField(max_length=200)
+    universities = models.ManyToManyField(University,related_name='universityRegion')
+    RegionStaff = models.ManyToManyField(User,related_name='Region_Staff',blank=True,null=True)
+
+    def __str__(self):
+        return self.name
+
+class UniversityEmailFormats(baseModel):
+    """
+    Lists of formats to be used to match a students email address
+    with a university. If no match is found here, then the students
+    request for a login will be rejected
+    """
+    university = models.ForeignKey(University,related_name='emailFormats')
+    format = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.format
+
+class Student(baseModel):
+    """
+    Student class holding basic information on the student
+    """
+    user = models.OneToOneField(User,related_name='student_details')
+    address =models.TextField(null=True,blank=True)
+    tagline = models.CharField(max_length=500,blank=True)
+    description = models.TextField()
+    avatarImage = models.ImageField(upload_to='images/avatars')
+    settings = JSONField(blank=True,null=True)
+
+    def __str__(self):
+        return "%s(%s)" % (self.user.get_username(),self.user.get_full_name())
+
+class BusinessUser(Student):
+    """
+    business user details
+    """
+    user = models.OneToOneField(User,related_name='business_user')
+    jobTitle = models.CharField(max_length=200,blank=True,null=True)
+    department = models.CharField(max_length=200,blank=True,null=True)
+
+class Business(baseModel):
+    """
+    object holding details on businesses in the system and
+    which users are part of that business
+    """
+    name = models.CharField(max_length=200)
+    address = models.TextField(blank=True)
+    description = models.TextField(blank=True)
+    logo = models.ImageField(upload_to='images/logos',null=True,blank=True)
+    website = models.URLField(null=True,blank=True)
+    staff = models.ManyToManyField(User, related_name='business_staff')
+
+    def __str__(self):
+        return self.name
---
+++ promozo/core/tests.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+
+# Create your tests here.
+# todo Get Current Users user details along with user Type and univeristy/business linked to them assuming they are logged in
+#todo attempt to get the current users details when the user is not authenticated and logged in
+#todo reset a users password for an authenitucated user
+#todo send a password reset email to an unauthenticated user with a valid email
+#todo attempt to send a password reset email to a user with an invalid email
+#todo register a new student assuming their email is valid for one of the universities in the system
+#todo attempt to register a student with an invalid email
+#todo authenticate and log a user into the system with valid credentials
+#todo attempt to authenticate a user with invalid credentials
+#todo log the current user out of the system
+#todo modify a student users information
+#todo attempt to modify a students details when the user is not authenticated
+#todo modify a business users information
+#todo for a business user, modify the business details
+# todo for the current user, get a list of all their uploaded documentation
