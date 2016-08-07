b115228cb2de20150a880580244dae63cb6ac03d
MarcNealer <marcnealer@gmail.com>
2016-08-07 16:07:54 +0700

    Tests and Questions UI completed Changes to the layout to curve the card boxes and inner toolbars

--- promazo/static/scripts/controllers/TestsController.js
+++ promazo/static/scripts/controllers/TestsController.js
@@ -16,8 +16,14 @@ promazo.controller('TestsController',function( $scope,$log, TestsResource, Quest
     $scope.showCreateTest=false;
     $scope.showCreateQuestion=false;
     
-    $scope.newTest = new TestsResource();
-    $scope.newQuestion = new QuestionResource();
+    $scope.newTest = new TestsResource({user:$scope.currentUser.id});
+    $scope.newQuestion = new QuestionResource({user:$scope.currentUser.id});
+    
+    $scope.ConfirmDelete = $mdDialog.confirm()
+        .title('Are you sure you want to Delete this item')
+        .textContent('There is no undo, so please confirm before deletion')
+        .ok("It's ok, Delete it")
+        .cancel("'Oh no. don't do it");
     
     
     $scope.toggle_CreateTest = function(){
@@ -43,44 +49,67 @@ promazo.controller('TestsController',function( $scope,$log, TestsResource, Quest
     $scope.CreateTest = function(){
       $scope.newTest.$save(function(data){
           $scope.openToast('test saved');
-          $scope.newTest = new TestsResource();
-          $scope.MyTestsList = QuestionResource.query({user:$scope.currentUser.id});
+          $scope.newTest = new TestsResource({user:$scope.currentUser.id});
+          $scope.MyTestsList = TestsResource.query({user:$scope.currentUser.id});
           $scope.showCreateTest=false;
       })  
     };
     
     $scope.CreateQuestion = function(){
-      $scope.newTest.$save(function(data){
+      $scope.newQuestion.$save(function(data){
           $scope.openToast('test saved');
-          $scope.newTest = new TestsResource();
-          $scope.MyTestsList = QuestionResource.query({user:$scope.currentUser.id});
+          $scope.newQuestion = new QuestionResource({user:$scope.currentUser.id});
+          $scope.MyQuestionsList = QuestionResource.query({user:$scope.currentUser.id});
           $scope.showCreateQuestion=false;
       })  
     };
     
     $scope.EditTest = function(test){
         $scope.currentTest=test;
-        $scope.testsPage="editTest";
+        $scope.testsPage="EditTest";
     };
     
      $scope.EditQuestion = function(question){
         $scope.currentQuestion=question;
-        $scope.testsPage="editQuestion";
+        $scope.testsPage="EditQuestion";
     };
     
     
     $scope.UpdateTest = function(test) {
-        test.$update(function(data){
+        test.$update({id:test.id},function(data){
             $scope.openToast('test updated');
             $scope.testsPage="tests";
         })
     };
     
     $scope.UpdateQuestion = function(question) {
-        question.$update(function(data){
+        question.$update({id:question.id},function(data){
             $scope.openToast('question updated');
             $scope.testsPage="questions";
         })
+    };
+
+    $scope.DeleteTest = function(test){
+        $mdDialog.show($scope.ConfirmDelete)
+            .then(function(){
+                test.$delete({id:test.id}, function (data) {
+                        $scope.MyTestsList = TestsResource.query({user:$scope.currentUser.id});
+                        $scope.PublicTests = TestsResource.query({public:2});
+
+                })
+            })
+
+    };
+    $scope.DeleteQuestion = function(question){
+        $mdDialog.show($scope.ConfirmDelete)
+            .then(function(){
+                question.$delete({id:question.id}, function (data) {
+                        $scope.MyQuestionsList = QuestionResource.query({user:$scope.currentUser.id});
+                        $scope.PublicQuestions = QuestionResource.query({public:2});
+
+                })
+            })
+
     };
     
     
